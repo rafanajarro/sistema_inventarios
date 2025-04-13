704c41a8e476e230e102cdececd0f4ec76e8690b
rafanajarro <rafaelnajarro10@gmail.com>
2025-04-12 22:27:09 -0600

    proyecto inicial

---
+++ src/main/java/com/inventario/sistemainvetario/SistemainvetarioApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.sistemainvetario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemainvetarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemainvetarioApplication.class, args);
+	}
+
+}
